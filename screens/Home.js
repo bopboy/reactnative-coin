fb85d517f9a4e06f2dc5d0e5896f5b9e7ac4d875
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-27 19:37:03 +0900

    90 Coin (Home) 스크린 (리액트 쿼리)

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useQuery } from 'react-query';
 import styled from 'styled-components/native'
 import { BLACK_COLOR } from "../colors";
 import auth from '@react-native-firebase/auth'
+import { coins } from '../api';
+import { ActivityIndicator } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 const Container = styled.View`
   background-color: ${BLACK_COLOR};
@@ -17,18 +21,53 @@ const Btn = styled.TouchableOpacity`
     border-width: 1px;
     border-radius: 20px;
     border-color: rgba(255, 255, 255, 0.5);
+    margin-top: 20px;
 `
 const BtnText = styled.Text`
     color: white;
     font-size: 16px;
 `
+const Loader = styled.View`
+    flex: 1;
+    background-color: ${BLACK_COLOR};
+    justify-content: center;
+    align-items: center;
+`
+const Coin = styled.View`
+    align-items: center;
+`
+const CoinName = styled.Text`
+    color: white;
+`
+const CoinSymbol = styled.Text`
+    color: white;
+`
 const onSubmitEditing = async () => {
     await auth().signOut()
 }
 const Home = () => {
+    const { isLoading, data } = useQuery("coins", coins)
+    const [cleanData, setCleanData] = useState([])
+    useEffect(() => {
+        if (data)
+            setCleanData(data.filter(coin => coin.rank != 0 && coin.is_active && !coin.is_new))
+    }, [data])
+    if (isLoading) {
+        return (<Loader><ActivityIndicator color="white" size="large" /></Loader>)
+    }
     return (
         <Container>
-            <Text>Home</Text>
+            <FlatList
+                data={cleanData}
+                numColumns={5}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <Coin>
+                        <CoinName>{item.name}</CoinName>
+                        <CoinSymbol>{item.symbol}</CoinSymbol>
+                    </Coin>
+                )}
+            />
             <Btn onPress={onSubmitEditing}>
                 <BtnText>Sign Out</BtnText>
             </Btn>
